4c388d692c48eb9e5e77c00c5b9dac28e7720c0a
Justin Abellera <justin.c.abellera@gmail.com>
2022-08-15 23:48:38 -0700

    post successful_ClassBased

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 // Suggested initial states
 const initialMessage = ''
@@ -80,17 +81,67 @@ export default class AppClass extends React.Component {
   }
 
   getNextIndex = (direction) => {
-    if(direction === 'up' && this.state.index !== 0 && this.state.index !== 1 && this.state.index !== 2 ) {
+    if(direction === 'up') {
+      if(this.state.index !== 0 && this.state.index !== 1 && this.state.index !== 2) {
+        this.setState({...this.state, steps: this.state.steps + 1})
       return this.state.index - 3
-    } else if(direction === 'down' && this.state.index !== 6 && this.state.index !== 7 && this.state.index !== 8) {
-      return this.state.index + 3
-    } else if(direction === 'left' && this.state.index !== 0 && this.state.index !== 3 && this.state.index !== 6) {
-      return this.state.index - 1
-    } else if(direction === 'right' && this.state.index !== 2 && this.state.index !== 5 && this.state.index !== 8) {
-      return this.state.index + 1
-    } else {
-      return this.state.index
+      } else if(this.state.index === 0 || this.state.index === 1 || this.state.index === 2) {
+        this.setState({...this.state, message: "You can't go up"})
+        return this.state.index
+      }
+    } else if(direction === 'down') {
+      if(this.state.index !== 6 && this.state.index !== 7 && this.state.index !== 8) {
+        this.setState({...this.state, steps: this.state.steps + 1})
+        return this.state.index + 3
+      } else if(this.state.index === 6 || this.state.index === 7 || this.state.index === 8) {
+        this.setState({...this.state, message: "You can't go down"})
+        return this.state.index
+      }
+    } else if(direction === 'left') {
+      if(this.state.index !== 0 && this.state.index !== 3 && this.state.index !== 6) {
+        this.setState({...this.state, steps: this.state.steps + 1})
+        return this.state.index - 1
+      } else if(this.state.index === 0 || this.state.index === 3 || this.state.index === 6) {
+        this.setState({...this.state, message: "You can't go left"})
+        return this.state.index
+      }
+    } else if(direction === 'right') {
+      if(this.state.index !== 2 && this.state.index !== 5 && this.state.index !== 8) {
+        this.setState({...this.state, steps: this.state.steps + 1})
+        return this.state.index + 1
+      } else if(this.state.index === 2 || this.state.index === 5 || this.state.index === 8) {
+        this.setState({...this.state, message: "You can't go right"})
+        return this.state.index
+      }
     }
+
+    // if(direction === 'up' && this.state.index !== 0 && this.state.index !== 1 && this.state.index !== 2 ) {
+    //   this.setState({...this.state, steps: this.state.steps + 1})
+    //   return this.state.index - 3
+    // } else if(direction === 'down' && this.state.index !== 6 && this.state.index !== 7 && this.state.index !== 8) {
+    //   this.setState({...this.state, steps: this.state.steps + 1})
+    //   return this.state.index + 3
+    // } else if(direction === 'left' && this.state.index !== 0 && this.state.index !== 3 && this.state.index !== 6) {
+    //   this.setState({...this.state, steps: this.state.steps + 1})
+    //   return this.state.index - 1
+    // } else if(direction === 'right' && this.state.index !== 2 && this.state.index !== 5 && this.state.index !== 8) {
+    //   this.setState({...this.state, steps: this.state.steps + 1})
+    //   return this.state.index + 1
+    // } else if(direction === 'up' && this.state.index === 0 || this.state.index === 1 || this.state.index === 2) {
+    //   this.setState({...this.state, message: "You can't go up"})
+    //   return this.state.index
+    // } else if(direction === 'down' && this.state.index === 6 || this.state.index === 7 || this.state.index === 8) {
+    //   this.setState({...this.state, message: "You can't go down"})
+    //   return this.state.index
+    // } else if(direction === 'left' && this.state.index === 0 || this.state.index === 3 || this.state.index === 6) {
+    //   this.setState({...this.state, message: "You can't go left"})
+    //   return this.state.index
+    // }else if(direction === 'right' && this.state.index === 2 || this.state.index === 5 || this.state.index === 8) {
+    //   this.setState({...this.state, message: "You can't go right"})
+    //   return this.state.index
+    // }else {
+    //   return this.state.index
+    // }
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
@@ -100,7 +151,8 @@ export default class AppClass extends React.Component {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
     // debugger
-    this.setState({index: this.getNextIndex(evt), steps: this.state.steps + 1})
+    this.setState({...this.state, message: "You can't go left"})
+    this.setState({index: this.getNextIndex(evt)})
   }
 
   onChange = (evt) => {
@@ -110,7 +162,26 @@ export default class AppClass extends React.Component {
 
   onSubmit = (evt) => {
     // Use a POST request to send a payload to the server.
+    evt.preventDefault()
 
+    if(this.state.email === 'foo@bar.baz') {
+      this.setState({message: 'foo@bar.baz failure #71'})
+    } else {
+      const xy = this.getXY()
+    const data = {
+      x: xy.x,
+      y: xy.y,
+      steps: this.state.steps,
+      email: this.state.email
+    }
+    const dataString = JSON.stringify(data)
+    // console.log(dataString)
+    axios.post('http://localhost:9000/api/result', data) //Doesnt need stringified data
+      .then(res => {
+        this.setState({...this.state, message: res.data.message, email: initialEmail})
+      })
+      .catch(err => console.log(err.message))
+    }
   }
 
   render() {
@@ -131,7 +202,7 @@ export default class AppClass extends React.Component {
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
           <button id="left" onClick={() => this.move('left')}>LEFT</button>
@@ -140,9 +211,9 @@ export default class AppClass extends React.Component {
           <button id="down" onClick={() => this.move('down')}>DOWN</button>
           <button id="reset" onClick={() => this.reset()}>reset</button>
         </div>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <input id="email" type="email" placeholder="type email" value={this.state.email} onChange={this.onChange}></input>
-          <input id="submit" type="submit" onClick={() => onSubmit()}></input>
+          <input id="submit" type="submit"></input>
         </form>
       </div>
     )
